7069c034dd30ae72151abde11ce5dbafb1b8bc14
user1 <user1@ubuntu>
2017-06-25 07:36:45 -0700

    mod1

---
+++ test1_3/src/test1_3proj.cpp
@@ -0,0 +1,15 @@
+//============================================================================
+// Name        : test1_3proj.cpp
+// Author      : 
+// Version     :
+// Copyright   : Your copyright notice
+// Description : Hello World in C++, Ansi-style
+//============================================================================
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	cout << "!!!Hello World!!!" << endl; // prints !!!Hello World!!!
+	return 0;
+}
